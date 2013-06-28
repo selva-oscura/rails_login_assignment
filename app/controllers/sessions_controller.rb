fac2e47e60913765ce2804bb0ca195339ce88243
selva-oscura <selva_oscura@yahoo.com>
2013-06-28 15:17:09 -0700

    adjusted default path after log-in (sessions_controller.rb)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
   		render :new
   	else
   		sign_in user
-  		redirect_to user
+  		# redirect_to user
+  		redirect_to users_path
   	end
 
   end
